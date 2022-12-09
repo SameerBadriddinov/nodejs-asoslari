089450b08b02f83bef4b441bee78e303931e7c9d
Samar <samarbadriddinov.tv@gmail.com>
2022-12-09 14:11:01 +0200

    module path

---
+++ modules/path.js
@@ -0,0 +1,10 @@
+const path = require('path')
+
+// console.log(path.basename(__filename))
+// console.log(path.dirname(__filename))
+// console.log(path.extname(__filename))
+
+// console.log(path.parse(__filename))
+
+// console.log(path.join(__dirname, 'templates', 'index.html'))
+// console.log(path.resolve(__dirname, './templates', 'index.html'))
