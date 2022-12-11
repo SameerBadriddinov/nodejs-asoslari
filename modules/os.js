05c87581eee2b9143e736604c941226701bb3713
Samar <samarbadriddinov.tv@gmail.com>
2022-12-11 22:31:03 +0200

    module os

---
+++ modules/os.js
@@ -0,0 +1,19 @@
+const os = require('os')
+
+// platform
+// console.log(os.platform())
+
+// information
+// console.log(os.cpus())
+
+// architecture
+// console.log(os.arch())
+
+// free memory
+// console.log(os.freemem())
+
+// total memory
+// console.log(os.totalmem())
+
+// entry point
+console.log(os.homedir())
