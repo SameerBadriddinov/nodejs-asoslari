87506cf62379d76fd47bc3ca7f79f241e52b346f
Samar <samarbadriddinov.tv@gmail.com>
2022-12-12 22:10:10 +0200

    #14. HTML bilan ishlash

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
 const http = require('http')
+const fs = require('fs')
+const path = require('path')
 
 const server = http.createServer((req, res) => {
 	if (req.method === 'GET') {
 		res.writeHead(200, {'Content-Type': 'text/html'})
 
-		res.end(`
-			<h2>Send name</h2>
-			<form method="post" action="/">
-				<input name="name" placeholder="Enter your name" />
-				<button type="submit">Send name</button>
-			</form>
-		`)
+		if (req.url === '/') {
+			fs.readFile(path.join(__dirname, 'templates', 'index.html'), 'utf-8', (err, conten) => {
+				if (err) throw err
+				res.end(conten)
+			})
+		} else if (req.url === '/about') {
+			fs.readFile(path.join(__dirname, 'templates', 'about.html'), 'utf-8', (err, conten) => {
+				if (err) throw err
+				res.end(conten)
+			})
+		} else if (req.url === '/contact') {
+			fs.readFile(path.join(__dirname, 'templates', 'contact.html'), 'utf-8', (err, conten) => {
+				if (err) throw err
+				res.end(conten)
+			})
+		}
 	} else if (req.method === 'POST') {
 		const body = []
 		res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'})
